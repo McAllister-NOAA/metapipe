e09bf7444b036b247ac25e9340ba3b2caad7d41e
McAllister-NOAA <sean.mcallister@noaa.gov>
2020-03-31 10:24:15 -0700

    Fixes bugs in maps.R script

--- assets/maps.R
+++ assets/maps.R
@@ -18,6 +18,7 @@ library("ggrepel")
 library("marmap")
 library("scatterpie")
 library("janitor")
+library("ggpubr")
 
 setwd(as.character(args[1]))
 theme_set(theme_bw())
@@ -134,6 +135,54 @@ m_bath_sanslegend
 dev.off()
 
 #############Simple map with data points and pie charts
+
+#####Find repel coordinates
+#' Given a Set of Points and Box sizes, find locations
+#' Written by @zachp, updated by @slowkow
+findboxes <- function(
+  df, xcol, ycol,
+  box_padding_x, box_padding_y,
+  point_padding_x, point_padding_y,
+  xlim, ylim,
+  force = 1e-7, maxiter = 20000
+) {
+  
+  # x and y posiitons as a dataframe
+  posdf <- df[c(xcol, ycol)]
+  
+  # returnd a df where columns are points
+  boxdf <- apply(posdf, 1, function(row) {
+    xval <- row[xcol]
+    yval <- row[ycol]
+    return(c(
+      xval - box_padding_x / 2,
+      yval - box_padding_y / 2,
+      xval + box_padding_x / 2,
+      yval + box_padding_y / 2
+    ))
+  })
+  # columns are x1,y1,x2,y2
+  boxmatrix <- as.matrix(t(boxdf))
+  
+  moved <- ggrepel:::repel_boxes(
+    data_points = as.matrix(posdf),
+    point_padding_x = point_padding_x,
+    point_padding_y = point_padding_y,
+    boxes = boxmatrix,
+    xlim = xlim,
+    ylim = ylim,
+    hjust = 0.5,
+    vjust = 0.5,
+    force = force,
+    maxiter = maxiter
+  )
+  
+  finaldf <- cbind(posdf, moved)
+  names(finaldf) <- c("x1", "y1", "x2", "y2")
+  return(finaldf)
+}
+#####################################End of functions
+
 filter_percent <- as.numeric(args[6])
 
 abundance_dat <- read.delim(as.character(args[5]), header=TRUE, stringsAsFactors=FALSE, check.names = FALSE)
@@ -142,7 +191,7 @@ abundance_dat$readSum <- rowSums(abundance_dat[, -1])
 abundance_dat <- abundance_dat %>% mutate_at(vars(-Sample, -readSum), funs(100*./readSum))
 abundance_dat <- abundance_dat %>% select(-readSum)
 
-if (sitelabelFlag == TRUE) {  ##STOP HERE
+if (sitelabelFlag == TRUE) {
   abundance_meta_join <- left_join(select(sample_metadata, Sample, sites), abundance_dat, by = "Sample")
   abundance_meta_join <- abundance_meta_join %>% select(-Sample)
   rep_group <- abundance_meta_join %>% group_by(sites) %>% 
@@ -198,6 +247,15 @@ plasma_pal <- c(viridis::plasma(n = number_observations - 1), "lightgrey")
 
 legend_title <- paste("Taxonomic Group (>", filter_percent, "%)", sep = "")
 
+repelled_rep_group <- findboxes(rep_group, xcol = "long", ycol = "lat",
+                                box_padding_x = as.numeric(pie_scale/5), box_padding_y = as.numeric(pie_scale/5),
+                                point_padding_x = as.numeric(pie_scale/5), point_padding_y = as.numeric(pie_scale/5),
+                                xlim = c(min_long-0.5, max_long+0.5), ylim = c(min_lat-0.5, max_lat+0.5))
+
+test_rep_group <- rep_group
+test_rep_group[,3] <- repelled_rep_group[,3]
+test_rep_group[,2] <- repelled_rep_group[,4]
+
 m_pies <- ggplot(data = world_of_interest, aes(x=long, y=lat)) +
   geom_polygon(aes(group = group), 
                fill = "cornsilk",
@@ -215,18 +273,42 @@ m_pies <- ggplot(data = world_of_interest, aes(x=long, y=lat)) +
   theme(panel.grid.major = element_line(color = gray(.5), linetype = "dashed", size = 0.1), 
         panel.background = element_rect(fill = "aliceblue"))
 
+pies_legend <- get_legend(m_pies)
+pies_legend <- as_ggplot(pies_legend)
+
+pdf(file='mapPies_legend.pdf')
+pies_legend
+dev.off()
+
+m_pies <- m_pies + theme(legend.position='none')
+
 pdf(file='mapPies.pdf')
 m_pies
 dev.off()
   
+sites_mod <- sites
+sites_mod[,2] <- test_rep_group[,3]
+sites_mod[,3] <- test_rep_group[,2]
 
+m_pies_scatt <- ggplot(data = world_of_interest, aes(x=long, y=lat)) +
+  geom_polygon(aes(group = group), 
+               fill = "cornsilk",
+               color = "black") +
+  coord_map(xlim = c(min_long-0.5, max_long+0.5),
+            ylim = c(min_lat-0.5, max_lat+0.5), 
+            clip = "on", projection = "orthographic") +
+  geom_text(data = sites_mod, aes(x = long, y = lat, label = labels), nudge_y = pie_scale/50) +
+  geom_point(data = sites, aes(x = long, y = lat), shape = 21, fill = "red") +
+  geom_segment(data = repelled_rep_group, aes(x = x1, y = y1, xend = x2, yend = y2)) +
+  geom_scatterpie(data = test_rep_group, aes(x=long, y=lat, group=labels),
+                  cols = colnames(test_rep_group[,4:ncol(test_rep_group)]),
+                  pie_scale = pie_scale,
+                  sorted_by_radius = TRUE, color = NA) +
+  scale_fill_manual(values = plasma_pal, name = legend_title) +
+  xlab("Longitude") + ylab("Latitude") +
+  theme(panel.grid.major = element_line(color = gray(.5), linetype = "dashed", size = 0.1), 
+        panel.background = element_rect(fill = "aliceblue"), legend.position='none')
 
-
-
-
-
-
-
-
-
-
+pdf(file='mapPies_scattered.pdf')
+m_pies_scatt
+dev.off()
